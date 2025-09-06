0e357350f377ffe8fc798943aa33cd437c66a9af
Streamtg <202310475@issu.edu.do>
2025-09-06 17:10:29 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-# lanza zrok access en bg y luego node
-zrok access private 63wdti52mg49 --bind 127.0.0.1:8080 &
-sleep 5        # da tiempo a que el socket esté listo
-npm start
+# 1) lanza zrok en bg (desde el repo)
+./zrok access private 63wdti52mg49 --bind 127.0.0.1:8080 &
+sleep 5
+# 2) arranca el proxy
+exec npm start
