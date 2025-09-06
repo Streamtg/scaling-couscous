e4694883f7628e1d6718dd157da0d568999ee301
Streamtg <202310475@issu.edu.do>
2025-09-06 17:17:38 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # 1) lanza zrok en bg (desde el repo)
-zrok enable 63wdti52mg49
+./zrok enable 63wdti52mg49
 # copia el token que aparece (empieza por ghp_… o similar)
 ./zrok access private 63wdti52mg49 --bind 127.0.0.1:8080 &
 sleep 5
