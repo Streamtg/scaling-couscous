98aebf907e8afbd3809a35a6fa2b9876e2bac81c
Streamtg <202310475@issu.edu.do>
2025-09-06 17:08:27 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/env bash
-set -ex
-# 1) deps
-apt-get update && apt-get install -y wget tar
-# 2) zrok
-ZROK_VER=1.1.3
-wget -qO- https://github.com/openziti/zrok/releases/download/v${ZROK_VER}/zrok_${ZROK_VER}_linux_amd64.tar.gz \
-  | tar -xz -C /usr/local/bin
-chmod +x /usr/local/bin/zrok
-# 3) node deps
-npm install
+# descarga el binario
+wget https://github.com/openziti/zrok/releases/download/v1.1.3/zrok_1.1.3_linux_amd64.tar.gz
+tar -xzf zrok_1.1.3_linux_amd64.tar.gz   # extrae solo el ejecutable 'zrok'
+rm zrok_1.1.3_linux_amd64.tar.gz CHANGELOG.md LICENSE README.md
+chmod +x zrok
