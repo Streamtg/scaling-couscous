18fe579ec6e6f24d05b8ed21c9483fcc2d3235ff
Streamtg <202310475@issu.edu.do>
2025-09-06 17:05:53 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # lanza zrok access en bg y luego node
-zrok access private wvszln4dyz9q --bind 127.0.0.1:9191 &
+zrok access private 63wdti52mg49 --bind 127.0.0.1:8080 &
 sleep 5        # da tiempo a que el socket esté listo
 npm start
