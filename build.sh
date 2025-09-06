9a1bacb960eadeeb87183d315fee4ba926f50221
Streamtg <202310475@issu.edu.do>
2025-09-06 17:17:03 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ wget https://github.com/openziti/zrok/releases/download/v1.1.3/zrok_1.1.3_linux_
 tar -xzf zrok_1.1.3_linux_amd64.tar.gz   # extrae solo el ejecutable 'zrok'
 rm zrok_1.1.3_linux_amd64.tar.gz CHANGELOG.md LICENSE README.md
 chmod +x zrok
-zrok enable 63wdti52mg49
+./zrok enable 63wdti52mg49
